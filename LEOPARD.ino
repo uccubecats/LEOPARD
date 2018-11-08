e0e5ee201bf714be70e46ba2185ce88492ef4b28
Grace Gamstetter <grace.gamstetter@gmail.com>
2018-11-07 20:26:35 -0500

    Add in extra loops

--- LEOPARD.ino
+++ LEOPARD.ino
@@ -9,7 +9,7 @@
 #include "leopard_common.h"
 #include "packet.h"
 #include "payload.h"
-#include <SD.h>
+#include "SD.h"
 #include "SparkFunLSM9DS1.h"
 #include <Time.h>
 #include <TimeLib.h>
@@ -19,40 +19,14 @@ void setup()
 {
     // DEBUG
     Serial.begin(9600);
-    
+    Serial.println(F("Begin execution."));
 
-    /**************** Power / Attitude System ****************/
-
-    // TODO: Uncomment
     // Begin I2C Interface
-    // Wire.begin();
+    // Used by ADCS and Power sensors
+    Wire.begin();
 
-
-    /******************** Communications ********************/
-
-    // TODO: Setup pins for Hamshield
-
-
-    /******************** Payload Sensors ********************/
-
-    // Set up the Mux "select configuration" pins as outputs:
-    pinMode(PAYLOAD_S0_PIN, OUTPUT);
-    digitalWrite(PAYLOAD_S0_PIN, HIGH);
-    pinMode(PAYLOAD_S1_PIN, OUTPUT);
-    digitalWrite(PAYLOAD_S1_PIN, HIGH);
-    pinMode(PAYLOAD_S2_PIN, OUTPUT);
-    digitalWrite(PAYLOAD_S2_PIN, HIGH);
-
-    // Set up Z pin as the analog input
-    pinMode(PAYLOAD_Z_PIN, INPUT);
-
-    // TODO: Set up a digital pin to manage current allocation to payload sensors
-
-
-    /******************** Storage Management ********************/
-
-    // Set up pin for SD card
-    // Automatically configures pinmode for standard SPI configuration (pins 10-13)
+    // Begin SPI Interface - auto configured by SD.begin()
+    // Initialize SD card reader/writer board
     boolean sd_init_success = SD.begin();
 
     // DEBUG
@@ -60,48 +34,43 @@ void setup()
     {
         Serial.println(F("SD init failed"));
     }
-    else
-    {
-        Serial.println(F("SD init succeeded"));
-    }
+
+    // Set up the Payload Mux "select configuration" pins as outputs:
+    pinMode(PAYLOAD_S0_PIN, OUTPUT);
+    digitalWrite(PAYLOAD_S0_PIN, HIGH);
+    pinMode(PAYLOAD_S1_PIN, OUTPUT);
+    digitalWrite(PAYLOAD_S1_PIN, HIGH);
+    pinMode(PAYLOAD_S2_PIN, OUTPUT);
+    digitalWrite(PAYLOAD_S2_PIN, HIGH);
+
+    // Set up Z pin as the analog input for Payload Mux
+    pinMode(PAYLOAD_Z_PIN, INPUT);
+
+    // TODO: Set up a digital pin to manage current allocation to payload sensors
+
 }
 
 void loop() 
 {
+    dataCollectionLoop();
+    commsLoop();
+    powerLoop();
+}
+
+void dataCollectionLoop()
+{
     LSM9DS1 adcs;
     Payload payload;
+    uint16_t payload_values[8];
+    packet::DataCollection dataCollection;
 
     // Set timestamp to current time (UTC)
-    unsigned long timestamp = (unsigned long) now();
-
-
-    /************************ Power Management ************************/
-
-    // TODO: Determine amount of power on battery
-    // TODO: Determine if system needs to go into different power mode and take action accordingly
-
-
-    /******************** Packetization ********************/
-
-
-    /******************** Communications ********************/
-
-    // TODO: Determine if packet was received, and analyze it if applicable
-    // TODO: Determine if packet should be sent and take action if applicable
-    // TODO: Determine if toggle Tx/Rx is in order and take action if it is so
-
-
-    /******************** Data Collection ********************/
+    uint32_t timestamp = (uint32_t) now();
 
     // Read current values from the ADCS sensor
     adcs.readGyro();
     adcs.readMag();
     adcs.readAccel();
-    adcs.readTemp();
-
-    // Read values for 3 axis data for gyroscope, magnetometer, accelerometer,
-    // and for temperature of the chip.
-    //adcs.temperature, adcs.gx, adcs.gy, adcs.gz, adcs.mx, adcs.my, adcs.mz, adcs.ax, adcs.ay, adcs.az;
 
     // Configure Mux Pins for current payload sensor and read output
     for (byte pin=0; pin<=7; pin++)
@@ -112,23 +81,44 @@ void loop()
         // Read input from current selected sonsor and convert value to volt value
         float reading = analogRead(PAYLOAD_Z_PIN) * 5.0 / 1023.0;
 
-        // Store current sensor value in payload vector
-        payload.setPayloadReading(pin, reading);
+        // Store current sensor value in collection struct
+        payload_values[pin] = reading;
     }
 
-
-    /******************** Storage Management ********************/
+    // Save all recorded values to dataCollection
+    dataCollection.timestamp_collection = timestamp;
+    dataCollection.gyro_x, dataCollection.gyro_y, dataCollection.gyro_z = adcs.gx, adcs.gy, adcs.gz;
+    dataCollection.mag_x, dataCollection.mag_y, dataCollection.mag_z = adcs.mx, adcs.my, adcs.mz;
+    dataCollection.accel_x, dataCollection.accel_y, dataCollection.accel_z = adcs.ax, adcs.ay, adcs.az;
+    dataCollection.sensor_0 = payload_values[0];
+    dataCollection.sensor_1 = payload_values[1];
+    dataCollection.sensor_2 = payload_values[2];
+    dataCollection.sensor_3 = payload_values[3];
+    dataCollection.sensor_4 = payload_values[4];
+    dataCollection.sensor_5 = payload_values[5];
+    dataCollection.sensor_6 = payload_values[6];
+    dataCollection.sensor_7 = payload_values[7];
     
     // Save filename as current timestamp
-    String filename = (String)(timestamp) + ".txt";   // TODO: Convert timestamp to strong
+    String filename = QUEUE_DIR + (String)(timestamp) + ".txt";
     File data_file = SD.open(filename, FILE_WRITE);
     //data_file.println("[%i, %i, %i]", adcs.gx, adcs.gy, adcs.gz);
     data_file.close();
 
-
-    /******************** Other ********************/
-
     // Pause 1 minute between each loop iteration
     delay(60000);
 
+}
+
+void commsLoop()
+{
+    // TODO: Determine if packet was received, and analyze it if applicable
+    // TODO: Determine if packet should be sent and take action if applicable
+    // TODO: Determine if toggle Tx/Rx is in order and take action if it is so
+}
+
+void powerLoop()
+{
+    // TODO: Determine amount of power on battery
+    // TODO: Determine if system needs to go into different power mode and take action accordingly
 }
